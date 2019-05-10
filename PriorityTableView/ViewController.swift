cbfac78a5c2fcc19bd1593ce1db74a2eea44af5e
Egon Fiedler <efiedler93@gmail.com>
2019-05-09 21:27:39 -0700

    Empty started project

---
+++ PriorityTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PriorityTableView
+//
+//  Created by Egon Fiedler on 5/9/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
