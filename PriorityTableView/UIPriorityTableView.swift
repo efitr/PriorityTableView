2b291b0f178d07143e1ab4da94190d42673805a6
Egon Fiedler <efiedler93@gmail.com>
2019-05-09 22:05:46 -0700

    Basic skeleton of a tableview

---
+++ PriorityTableView/UIPriorityTableView.swift
@@ -0,0 +1,19 @@
+//
+//  UIPriorityTableView.swift
+//  PriorityTableView
+//
+//  Created by Egon Fiedler on 5/9/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class UIPriorityTableView: UIViewController {
+
+    var tableView: UITableView!
+
+}
+
+extension UIPriorityTableView: UITableViewDelegate, UITableViewDataSource {
+    
+}
